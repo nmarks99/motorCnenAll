99d106468f8ce536cf819d58be90a2ccedde6f67
nmarks99 <nmarks@anl.gov>
2025-01-09 12:00:17 -0600

    sub record to call enable/disable

--- SPiiPlusEnableAllApp/src/SPiiPlusEnableAll.cpp
+++ SPiiPlusEnableAllApp/src/SPiiPlusEnableAll.cpp
@@ -7,6 +7,7 @@
 #include <epicsExport.h>
 #include <errlog.h>
 #include <iocsh.h>
+#include <registryFunction.h>
 // #include <dbDefs.h>
 // #include <cantProceed.h>
 
@@ -36,9 +37,7 @@ static chid get_chid(const std::string &record_name) {
 
 // User can specify specific motors in the form of a comma separated list of motor names.
 // It is left to the user to ensure the requested motors actually exist
-std::vector<std::string> parse_motor_list(const std::string &prefix, const std::string &motor_list) {
-
-    std::vector<std::string> out;
+static std::vector<std::string> parse_motor_list(const std::string &prefix, const std::string &motor_list) {
 
     // remove whitespace
     std::string result = motor_list;
@@ -48,9 +47,10 @@ std::vector<std::string> parse_motor_list(const std::string &prefix, const std::
     std::regex re(",");
     std::sregex_token_iterator begin(result.begin(), result.end(), re, -1);
     std::sregex_token_iterator end;
-    std::vector<std::string> out_tmp = {begin, end};
 
-    for (const auto &s: out_tmp) {
+    // prepend IOC prefix to each motor record name
+    std::vector<std::string> out;
+    for (const auto &s: std::vector<std::string>({begin, end})) {
         out.push_back(prefix+s);
     }
 
@@ -58,12 +58,10 @@ std::vector<std::string> parse_motor_list(const std::string &prefix, const std::
 
 }
 
-// static long create_pv_monitor(int event_type, chid channel_id) {
-    // // status = ca_add_event(DBR_SHORT, channelID, &dmov_handler, &(motorArray[motor_index].index), 0);
-// }
-
-// must be done after IOC init
-void SPiiPlusEnableAllInit(const std::string &prefix, const std::string &motor_list_str) {
+// Must be called from iocsh after iocInit
+// prefix and motor_list are optional and if either are empty, all
+// motor records in the IOC will be connected to
+void SPiiPlusEnableAllInit(const char *prefix, const char *motor_list) {
     static bool initialized = false;
     if (initialized) {
         errlogPrintf("SPiiPlusEnableAll already initialized\n");
@@ -73,8 +71,10 @@ void SPiiPlusEnableAllInit(const std::string &prefix, const std::string &motor_l
     // Create channel access context
     SEVCHK(ca_context_create(ca_enable_preemptive_callback), "SPiiPlusEnableAllInit: ca_context_create() error");
 
+    // Get the motor record names
     std::vector<std::string> record_names;
-    if (motor_list_str.length() == 0) {
+    if (motor_list == NULL || prefix == NULL) {
+        // get all motor records in this IOC
         record_names = get_record_names();
         initialized = true;
         if (record_names.size() == 0) {
@@ -82,30 +82,48 @@ void SPiiPlusEnableAllInit(const std::string &prefix, const std::string &motor_l
             return;
         }
     } else {
-        record_names = parse_motor_list(prefix, motor_list_str);
+        // get only requested motor records
+        record_names = parse_motor_list(prefix, motor_list);
         if (record_names.size() == 0) {
-            errlogPrintf("SPiiPlusEnableAllInit: Error parsing requested motors:\n%s\n", motor_list_str.c_str());
+            errlogPrintf("SPiiPlusEnableAllInit: Error parsing requested motors:\n%s\n", motor_list);
             return;
         }
     }
 
+    // Connect to each CNEN PV and store the channel ID
     for (const auto &rec : record_names) {
         errlogPrintf("Record: %s\n", rec.c_str());
         chid_list.push_back(get_chid(rec+".CNEN"));
     }
 }
 
-void SPiiPlusEnableAll() {
+
+// Enables all requested motors (CNEN=1)
+// Registered as function with epicsRegisterFunction and called by sub record
+long SPiiPlusEnableAll() {
     short val = 1;
     for (size_t i = 0; i < chid_list.size(); i++) {
         ca_array_put(DBF_SHORT, 1, chid_list.at(i), &val);
         ca_flush_io();
     }
+    return 0;
 }
 
-//
+// Disables all requested motors (CNEN=0)
+// Registered as function with epicsRegisterFunction and called by sub record
+long SPiiPlusDisableAll() {
+    short val = 0;
+    for (size_t i = 0; i < chid_list.size(); i++) {
+        ca_array_put(DBF_SHORT, 1, chid_list.at(i), &val);
+        ca_flush_io();
+    }
+    return 0;
+}
+
+
+// ------------------
 // iocsh registration
-//
+// ------------------
 static const iocshArg SPiiPlusEnableAllInitArg0 = {"IOC prefix", iocshArgString};
 static const iocshArg SPiiPlusEnableAllInitArg1 = {"Motors list", iocshArgString};
 static const iocshArg *const SPiiPlusEnableAllInitArgs[2] = {
@@ -133,4 +151,6 @@ static void SPiiPlusEnableAllRegister(void) {
 extern "C" {
 epicsExportRegistrar(SPiiPlusEnableAllInitRegister);
 epicsExportRegistrar(SPiiPlusEnableAllRegister);
+epicsRegisterFunction(SPiiPlusEnableAll);
+epicsRegisterFunction(SPiiPlusDisableAll);
 }
